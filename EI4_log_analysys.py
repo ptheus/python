12e4255d6f391addadec1d53790810abb6f7fefc
xingzhe <xingzhe_sust@163.com>
2014-10-20 18:17:56 +0800

    add progressbar

--- EI4_log_analysys.py
+++ EI4_log_analysys.py
@@ -1,6 +1,12 @@
 import time, sys
+import gl
 from progressbar import Percentage, ProgressBar, Bar, FormatLabel
 # deal with two list and delete have matched items
+def usage():
+    print '''Usage:''' + sys.argv[0] + ''' [-i] file [-o output] [-f leach argument] [-m match argument]
+   You must input file to deal with. and you can choose -o to assign the output name. 
+    The filter argument which is use -f or -m is optional. if not , the argument will be use default argument.
+    The default argument is: '''  + sys.argv[0] + ''' -o result_record.txt -f |type=double_quote| |type=double_quote_ack| -m |bid_quote_id |ask_quote_id |feedcode'''
 
 def right_swich(time):
     hour = int(time[0:2])
@@ -12,31 +18,51 @@ def right_swich(time):
     
 def abstract_list(line):
     tmp_list = []
-    tmp = line[line.find(Arg_mA):]
-    tmp_list.append(line[line.find(Arg_mA) - 1:tmp.find(']') + 1)
-    tmp = line[line.find(Arg_mB):]
-    tmp_list.append(line[line.find(Arg_mB) - 1:tmp.find(']') + 1)
-    tmp = line[line.find(Arg_mC):]
-    tmp_list.append(line[line.find(Arg_mC) - 1:tmp.find(']') + 1)
+    if gl.file_flag = 0:
+        tmp = line[line.find(gl.Arg_mA):]
+        tmp_list.append(line[line.find(gl.Arg_mA):tmp.find(']'))
+        tmp = line[line.find(gl.Arg_mB):]
+        tmp_list.append(line[line.find(gl.Arg_mB):tmp.find(']'))
+        tmp = line[line.find(gl.Arg_mC):]
+        tmp_list.append(line[line.find(gl.Arg_mC):tmp.find(']'))
+    elif gl.file_flag = 1:
+        tmp = line[line.find(gl.Arg_mA):]
+        if tmp.find('|') >= 0:
+            tmp_list.append(line[line.find(gl.Arg_mA):tmp.find('|')])
+        else:
+            tmp_list.append(line[line.find(gl.Arg_mA):tmp.find('}')])
+        tmp = line[line.find(gl.Arg_mB):]
+        if tmp.find('|') >= 0:
+            tmp_list.append(line[line.find(gl.Arg_mB):tmp.find('|')])
+        else:                                  
+            tmp_list.append(line[line.find(gl.Arg_mB):tmp.find('}')])
+        tmp = line[line.find(gl.Arg_mC):]
+        if tmp.find('|') >= 0:
+            tmp_list.append(line[line.find(gl.Arg_mC):tmp.find('|')])
+        else:
+            tmp_list.append(line[line.find(gl.Arg_mC):tmp.find('}')])
     return tmp_list
-def get_time(line,sign):
-    if sign == 0:
+    
+
+def get_time(line):
+    if gl.file_flag == 0:
         tmp_line = line[line.find('AUDIT') - 16:line.find('AUDIT') - 1]
-    if sign == 1:
+    if gl.file_flag == 1:
         tmp_line = line[line.find('time') + 5:line.find('time') + 20]
     return tmp_line
+    
 # deal with have chosen items 
 def match_function()
     list_out_cursor = []
     list_in_cursor = []
     find_OK = False
-    for i in range(len(list_sed)):
-        list_tmp = abstract_list(list_sed[i])
-        for j in range(len(list_rev)):
-            if list_tmp == abstract_list(list_rev[j]):
+    for i in range(len(gl.list_sed)):
+        list_tmp = abstract_list(gl.list_sed[i])
+        for j in range(len(gl.list_rev)):
+            if list_tmp == abstract_list(gl.list_rev[j]):
                 find_OK = True
                 list_in_cursor.append(j)
-                rev_time = get_time(list_rev[j],file_flag)
+                rev_time = get_time(gl.list_rev[j])
                 break
         if find_OK:
             list_out_cursor.append(i)
@@ -44,46 +70,83 @@ def match_function()
             sed_time = get_time(list_sed[i],file_flag)
             list_tmp.append(rev_time)
             list_tmp.append(sed_time)
-            list_OK.append(list_tmp)
-    for id in list_out_cursor:
-        del list_sed[id]
-    for id in list_in_cursor:
-        del list_rev[id]
+            gl.list_OK.append(list_tmp)
+    for index in list_out_cursor:
+        del gl.list_sed[index]
+    for index in list_in_cursor:
+        del gl.list_rev[index]
+        
+def get_value(string):
+    if gl.file_flag = 0:
+        return string[string.find(":")+1:]
+    elif gl.file_flag = 1:
+        return string[string.find("=")+1:]
+    else:
+        return ''
+def count():
+    file_end_flag = 1
+    probar = 1
+    print "Begin analysis the log, please waiting...\n"
+    pbar.start()
+    for i in range(1,101):
+        while file_end_flag and probar:
+            line = src_handle.readline()
+            if len(line) == 0:
+                file_end_flag = 0
+            else:
+                gl.file_size_tmp += len(line)
+            if gl.file_size_tmp > (gl.file_size / 100) * i:
+                probar = 0
+                continue
+            if line.find(gl.Arg_A) >= 0:
+                gl.list_sed.append(line)
+            if line.find(gl.Arg_B) >= 0:
+                gl.list_rev.append(line)
+            if len(gl.list_sed) > 100 and len(gl.list_rev) > 100:
+                match_function()
+            if file_end_flag == 0:
+                match_function()
+                continue
+        pbar.update(i)
+        probar = 1
+    pbar.finish()
     
-# main
-max_time = 0
-min_time = 0
-average_time =0
-list_sed = []
-list_rev = []
-list_OK = []
-src_handle = open('', 'rU')
-tar_handle = open('', 'w')
-file_end_flag = False
-glb_line = ''
-file_flag = -1
-try:
-    for line in src_handle:
-        if len(line) == 0:
-            file_end_flag = True
-        if line.find(Arg_A) >= 0:
-            list_sed.append(line)
-        if line.find(Arg_B) >= 0:
-            list_rev.append(line)
-        if len(list_sed) > 1000 and len(list_rev) > 1000:
-            match_function()
-        if file_end_flag:
-            match_function()
-            break
+def gather_result():
+    print "Begin gather the result, please waiting...\n"
+    pbar.start()
     for cur_i in list_OK:
         for cur_j in range(len(cur_i) - 2):
-            glb_line = glbline + #添加提取函数。+ '\t'
+            glb_line = glbline + get_value(cur_i[cur_j]) + '\t'
         glb_line = glb_line + str(right_swich(cur_i[4]) - right_swich(cur_i[3])) + '\t\n'
         tar_handle.write(glb_line)
         glb_line = ''
-        
-            
-            
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+# main
+
+src_handle = open('GDK-Native01.ommTP.audit.log', 'rU')
+tar_handle = open('record.log', 'w')
+file_end_flag = False
+glb_line = ''
+
+widgets = [Percentage(), ' ', Bar(marker='|',left='|',right='|'),'[', FormatLabel('%(elapsed)s'), ']']
+pbar = ProgressBar(widgets=widgets, maxval=100)
+try:
+    count()
+    
+    
+    
+    
+    
+    
 finally:
     tar_handle.close()
     src_handle.close()
---
+++ gl.py
@@ -0,0 +1,17 @@
+# global variable
+max_time = 0
+min_time = 0
+average_time =0
+list_sed = []
+list_rev = []
+list_OK = []
+
+file_flag = -1
+file_size_tmp = 0
+file_size = 0
+
+Arg_A = ''
+Arg_B = ''
+Arg_mA = ''
+Arg_mB = ''
+Arg_mC = ''
